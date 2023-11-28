cb3f0c92e1cf9a8896d4a98e0ca5ad829a158e6a
nicolocamassa <camassanicolo03@gmail.com>
2023-11-28 15:52:37 +0100

    [ESERCIZIO FINITO] palindrome word checker added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 /* Inizializziamo la funzione */
-function checkPalindrome(word){
-        /* Verifichiamo la parola venga invertita */
-        console.log(word.split('').reverse().join('').toLowerCase())
+function checkPalindrome(word) {
+    /* Verifichiamo la parola venga invertita */
+    let reversedWord = word.split('').reverse().join('').toLowerCase();
+
+    /* Controllo della parola e output risultato*/
+    if(word === reversedWord){
+        console.log('La parola è palindroma')
+    }else{
+        console.log('La parola NON è palindroma')
+    }
 }
 
 /* Diamo in input la parola da verificare */
